376f86e1380f356256603f8bcac11bdf8f38e31a
Walter Gildersleeve <walter@gildersleeve.eu>
2018-05-28 07:39:34 +0200

    adding additional build info

--- streifen/ws/daybook/version.py
+++ streifen/ws/daybook/version.py
@@ -1,5 +1,13 @@
 
+import os
+
 class Version(object):
+    def __init__(self):
+        if ("DAYBOOK_API_BUILD" in os.environ):
+            self._build_number = os.environ["DAYBOOK_API_BUILD"]
+        else:
+            self._build_number = "-1"
+
     def message(self):
         return "Another Message"
 
@@ -7,7 +15,7 @@ class Version(object):
         return "0.0.2"
 
     def build_number(self):
-        return "1"
+        return self._build_number
 
     def author(self):
         return "Walter Gildersleeve"
