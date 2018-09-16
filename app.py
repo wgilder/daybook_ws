62e83743417c2c44aded721b1591228ffde217f1
Walter Gildersleeve <walter@gildersleeve.eu>
2018-09-16 19:36:28 +0200

    fixed to work with docker

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from ddtrace import tracer
 from ddtrace.contrib.flask import TraceMiddleware
 
 app = Flask(__name__)
+tracer.configure(hostname='172.17.0.1', port=8126)
 traced_app = TraceMiddleware(app, tracer, service="daybook_ws", distributed_tracing=False)
 
 from streifen.ws.daybook import routes
